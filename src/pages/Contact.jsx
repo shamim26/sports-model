aa8f24ebeb4c2d2fb909e1972f8315bd53bf1f3b
shamim26 <shamimreja101@gmail.com>
2023-09-30 14:09:37 +0600

    okk

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   return (
-    <div className=" container md:w-2/3 w-[95%] mx-auto border border-[#E5E5E5] rounded-lg p-14">
+    <div className=" container md:w-2/3 w-[95%] mx-auto border border-[#E5E5E5] rounded-lg p-4 md:p-14">
       <h1 className="text-center font-semibold text-lg md:text-4xl">
         GOT A QUESTION?
       </h1>
